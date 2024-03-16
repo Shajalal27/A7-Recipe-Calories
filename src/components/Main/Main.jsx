2c8778d7b6ce9bbe72f5e2785bb3374b4691f231
Shajalal27 <hpshajalal@gmail.com>
2024-03-16 13:01:31 +0600

    banner section responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,11 +4,11 @@ const Main = () => {
     return (
         <main>
             <div className="">
-                <div class="card w-[100%] relative">
+                <div class="card w-[100%]  relative my-6">
                     <figure><img src={banner_img} alt="" /></figure>
-                    <div class="card-body absolute top-[30%] space-y-8">
-                        <h2 class="card-title  text-5xl font-extrabold text-white pl-60 pr-60 items-center text-center ">Discover an exceptional cooking class tailored for you!</h2>
-                        <p class ="text-center text-white pl-36 pr-36">
+                    <div class="card-body absolute lg:top-[30%] md:top-[10%] -top-6 lg:space-y-8  space-y-1">
+                        <h2 class="card-title  lg:text-5xl md:text-5xl text-xl font-extrabold text-white lg:pl-60 pl-5 lg:pr-60 pr-5 lg:items-center text-center ">Discover an exceptional cooking class tailored for you!</h2>
+                        <p class ="text-center  text-white pl-36 pr-36">
                         Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.
                         </p>
                         <div class="card-actions justify-center">
